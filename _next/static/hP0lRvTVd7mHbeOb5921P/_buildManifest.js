8ae6f33df7272348afe75a84231c16cd54530bde
fuminya1109 <fuminya1109@users.noreply.github.com>
2022-01-19 04:07:58 +0000

    deploy: 1286aa5b6eb1491c5906ee878f21bdafed1a9a5b

---
+++ _next/static/hP0lRvTVd7mHbeOb5921P/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-d5d2f56849aa61e5.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
